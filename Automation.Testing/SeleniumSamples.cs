6214f00fc1e0b2e1c6b5897534414cc39146dda0
MyNameIsAina <tomsone.anna@gmail.com>
2021-05-04 19:24:44 +0300

    Testing webdriver connections

--- Automation.Testing/SeleniumSamples.cs
+++ Automation.Testing/SeleniumSamples.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Automation.Testing
@@ -11,8 +16,18 @@ namespace Automation.Testing
     public class SeleniumSamples
     {
         [DataTestMethod]
-        public void WebDriverSamples() { 
-            //TODO: add test logic here
+        public void WebDriverSamples() {
+            IWebDriver driver = new ChromeDriver();
+            Thread.Sleep(1000);
+            driver.Dispose();
+
+            driver = new FirefoxDriver();
+            Thread.Sleep(1000);
+            driver.Dispose();
+
+            driver = new InternetExplorerDriver();
+            Thread.Sleep(1000);
+            driver.Dispose();
         }
     }
 }
