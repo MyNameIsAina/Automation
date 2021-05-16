6b358384c5ba8e8e665762c55923445afa01bd71
MyNameIsAina <tomsone.anna@gmail.com>
2021-05-16 22:23:06 +0300

    Web driver factory and changed the net versions in project

--- Automation.Extensions/Components/WebDriverFactory.cs
+++ Automation.Extensions/Components/WebDriverFactory.cs
@@ -15,7 +15,7 @@ namespace Automation.Extensions.Components
     public class WebDriverFactory
     {
         private readonly DriverParams driverParams;
-        public WebDriverFactory(string driverParamsJson) 
+        public WebDriverFactory(string driverParamsJson)
             : this(LoadParams(driverParamsJson)) { }
 
         public WebDriverFactory(DriverParams driverParams)
@@ -27,15 +27,42 @@ namespace Automation.Extensions.Components
             }
         }
 
+        /*
+         * Returns a web driver based on what type of driver Source (Remote/Local Web) has been passed in the driverparams pbject
+         * 
+         * Returns Web Driver instance
+         */
+        public IWebDriver Get()
+        {
+            if (!string.Equals(driverParams.Source, "REMOTE", StringComparison.OrdinalIgnoreCase))
+            {
+                return GetDriver();
+            }
+            return GetRemoteDriver();
+        }
+
         // Local Web-drivers
-        private IWebDriver GetChrome() 
+        private IWebDriver GetChrome()
             => new ChromeDriver(driverParams.Binaries);
-        private IWebDriver GetFirefox() 
+        private IWebDriver GetFirefox()
             => new FirefoxDriver(driverParams.Binaries);
-        private IWebDriver GetInternetExplorer() 
-            => new InternetExplorerDriver(driverParams.Binaries);
+        private IWebDriver GetInternetExplorer()
+            => new InternetExplorerDriver(driverParams.Binaries, new InternetExplorerOptions {}, TimeSpan.FromMinutes(2));
         private IWebDriver GetEdge() 
             => new EdgeDriver(driverParams.Binaries);
+
+        // Web Driver switcher
+        private IWebDriver GetDriver() 
+        {
+            switch (driverParams.Driver.ToUpper())
+            {
+                case "CHROME": return GetChrome();
+                case "FIREFOX": return GetFirefox();
+                case "IE": return GetInternetExplorer();
+                case "EDGE": return GetEdge();
+                default: return GetChrome();
+            }
+        }
 
         //Remote Web-Drivers
         private IWebDriver GetReoteChrome() => new RemoteWebDriver(
@@ -50,6 +77,19 @@ namespace Automation.Extensions.Components
         private IWebDriver GetRemoteEdge() => new RemoteWebDriver(
             new Uri(driverParams.Binaries), new EdgeOptions()
             );
+
+        // Remote Driver switcher
+        private IWebDriver GetRemoteDriver()
+        {
+            switch (driverParams.Driver.ToUpper())
+            {
+                case "CHROME": return GetReoteChrome();
+                case "FIREFOX": return GetRemoteFirefox();
+                case "IE": return GetRemoteInternetExplorer();
+                case "EDGE": return GetRemoteEdge();
+                default: return GetReoteChrome();
+            }
+        }
 
         // Loads Json into DriverParams object
         private static DriverParams LoadParams(string driverParamsJson)
